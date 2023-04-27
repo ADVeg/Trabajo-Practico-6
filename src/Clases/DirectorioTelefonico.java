13f2ae644417f0c17d8e08a90bd62de8758d7c06
Vega Alberto Daniel <danielveg2013@gmail.com>
2023-04-27 13:21:43 -0300

    Primer comit
    Completado hasta ventana Buscar Cliente

---
+++ src/Clases/DirectorioTelefonico.java
@@ -0,0 +1,43 @@
+package Clases;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DirectorioTelefonico {
+    Map<Long,Cliente> clientes=new HashMap();
+
+    public DirectorioTelefonico() {
+    }
+
+    public Map<Long, Cliente> getClientes() {
+        return clientes;
+    }
+    
+    public boolean agregarCliente(Cliente cliente) {
+        if (clientes.containsKey(cliente.getTelefono())) {
+            return false;
+        }else{
+            clientes.put(cliente.getTelefono(), cliente);
+            return true;
+        }
+    }
+
+    public boolean buscarCliente(long telefono) {
+        if (clientes.containsKey(telefono)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void buscarTelefono() {
+    }
+
+    public void buscarClientes() {
+        
+    }
+
+    public void borrarCliente() {
+        
+    }
+}
